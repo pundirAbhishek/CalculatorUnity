27d480aa17c50d780d036e45a7b6782cfa5f5dca
AbhishekPundir <abhishekpundir526@gmail.com>
2024-06-28 23:22:02 +0530

    Added the basic UI elements in the game

---
+++ Assets/Calculator.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Calculator : MonoBehaviour
+{
+
+    public TMP_InputField firstNumber;
+    public TMP_InputField secondNumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
